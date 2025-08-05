0c022e8bbc2bc7192c838e93afbf64f30b2ccafc
saba farzanegan <saba.far.99@gmail.com>
2025-08-05 18:07:35 +0330

    add themeProvider to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
+import { Vazirmatn } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+import ThemeProvider from "@/provider/theme-provider";
+const vazir = Vazirmatn({
+  variable: "--font-vazir-sans",
   subsets: ["latin"],
 });
 
@@ -23,10 +19,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+    <html lang="fa" dir="rtl">
+      <ThemeProvider>
+        <body className={`${vazir.variable}  `}>
+          <main className="container">{children}</main>
+        </body>
+      </ThemeProvider>
     </html>
   );
 }
